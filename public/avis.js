47b530d4a9baedf91a8bff4cfff7859ce001bb1c
Jeremy <jeremysiviholya@outlook.fr>
2023-01-13 14:15:21 +0100

    update

--- public/avis.js
+++ public/avis.js
@@ -6,7 +6,16 @@ export function ajoutListenersAvis() {
            /* ... */
 
            const id = event.target.dataset.id;
-           fetch(`http://localhost:8081/pieces/${id}/avis`);
+           const reponse=await fetch(`http://localhost:8081/pieces/${id}/avis`);
+           const avis=await reponse.json();
+           const piecesElement=event.target.parentElement;
+
+           const avisElement=document.createElement('p');
+           for(let i=0;i<avis.length;i+=1)
+           {
+            avisElement.innerHTML += `${avis[i].utilisateur}: ${avis[i].commentaire} <br>`;
+           }
+           piecesElement.appendChild(avisElement);
       });
     }
 }
